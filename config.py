7c55f1c0152db89b4438ac164a9a51275a4ab646
dattudd <148096435+dattudd@users.noreply.github.com>
2023-10-16 22:11:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/c7fd42ae9b46a5381d5a5.jpg"
+    "START_IMG_URL", "https://graph.org/file/0de5cb8959e5451ee38ef.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
